951a73d7ddaa2358a4f7ca45014fb7387199a82f
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-02-05 15:06:49 +0100

    Handle ADD_TODO  action in todoApp reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,8 @@
-import { VisibilityFilters, SET_VISIBILITY_FILTER } from '../actions';
+import { 
+  VisibilityFilters, 
+  SET_VISIBILITY_FILTER,
+  ADD_TODO,
+  TOGGLE_TODO } from '../actions';
 
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
@@ -11,6 +15,17 @@ const todoApp = (state=initialState, action) => {
       return Object.assign({}, state, {
         visibilityFilter: action.filter
       });
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      });
+
     default:
       return state;
   }
